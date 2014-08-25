6abd717d3f42ddef4789e62d1b4ec3fbf42d32da
Prasetyo Wicaksono <prasetyo@jadenforex.com>
2014-08-25 16:32:00 +0700

    add new theme

--- config.php
+++ config.php
@@ -8,4 +8,18 @@ $config = array();
 $config['encryptionKey'] = '217f1lYIxOLaDvLeCvD7P(IYoMOhPE|Y[4)ZLtu9uvo34MaBEeiYLx(=89EBWcXl';
 
 // it is important to return the $config array!
+
+// set the theme for this installation
+$config['theme'] = 'blog';
+
+// set some defaults for the pages
+$config['date_format'] = 'jS F, Y'; // 11th November, 2013
+$config['pages_order_by'] = 'date'; // this is a blog so date ordering
+$config['pages_order'] = 'desc';
+
+// disable the cache for this theme since we are in development
+$config['plugins'] = array(
+  'philePhpFastCache' => array('active' => false),
+  'phileSimpleFileDataPersistence' => array('active' => false)
+);
 return $config;
